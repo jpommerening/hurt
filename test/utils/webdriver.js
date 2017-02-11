d25bd1810d22b85cfe1f5941bf97a3708c613d5b
Jonas Pommerening <jonas.pommerening@gmail.com>
2017-02-11 12:08:49 +0100

    Try to force selenium 3

--- test/utils/webdriver.js
+++ test/utils/webdriver.js
@@ -21,12 +21,19 @@ export function defaults() {
 export function capabilities() {
   const caps = {};
 
+  if (proces.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY) {
+    caps['seleniumVersion'] = '3.0.1';
+  }
   if (process.env.TRAVIS) {
     caps['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
     caps['build'] = process.env.TRAVIS_BUILD_NUMBER;
   }
   if (process.env.BROWSER) {
     caps['browserName'] = process.env.BROWSER;
+
+    if (process.env.BROWSER_VERSION) {
+      caps['version'] = process.env.BROWSER_VERSION;
+    }
   }
   if (process.env.PLATFORM) {
     caps['platform'] = process.env.PLATFORM;
