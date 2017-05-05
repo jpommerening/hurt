633c71d505d90141c1b925e6374035e0b6f30dd1
Jonas Pommerening <jonas.pommerening@gmail.com>
2017-05-05 22:39:33 +0200

    Handle cases where there is no __coverage__

--- test/e2e/browser/coverage.js
+++ test/e2e/browser/coverage.js
@@ -1,33 +1,15 @@
-/*global __coverage__*/
 import istanbul from 'istanbul-api';
+
+const __coverage__ = global.__coverage__ || (global.__coverage__ = {});
 
 export default function createReport(coverage) {
   if (!coverage || !__coverage__) {
     return;
   }
 
-  const coverageMap = istanbul.libCoverage.createCoverageMap();
+  const coverageMap = istanbul.libCoverage.createCoverageMap(coverage);
   const sourceMapStore = istanbul.libSourceMaps.createSourceMapStore();
-
-  Object.keys(coverage).forEach(filename => {
-    const cov = coverage[filename];
-
-      /*
-    if (cov.inputSourceMap) {
-      cov.inputSourceMap.sources = cov.inputSourceMap.sources.map(source => {
-        if (source.indexOf('!') !== -1) {
-          return source.split('!').pop();
-        }
-        return source;
-      });
-    }
-    */
-
-    coverageMap.addFileCoverage(cov);
-  });
-
   const remappedCoverage = sourceMapStore.transformCoverage(coverageMap).map;
-
   const currentCoverage = istanbul.libCoverage.createCoverageMap(__coverage__);
 
   remappedCoverage.files().forEach(filename => {
