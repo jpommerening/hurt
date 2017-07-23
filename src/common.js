74f785cd0cc9eb3f3d0a0e84b028bcc1f0db1804
Jonas Pommerening <jonas.pommerening@gmail.com>
2017-07-24 01:01:28 +0200

    Remove events from default bundle

--- src/common.js
+++ src/common.js
@@ -1,11 +1,10 @@
 import router from './router';
-import { mixin as events } from './events';
 import { mixin as named } from './named';
 import { mixin as regexpRoute } from './regexp-route';
 import { mixin as templateRoute } from './template-route';
 import { mixin as timeout } from './timeout';
 import { mixin as url } from './url';
 
-router.mixins.push( events, timeout, named, regexpRoute, templateRoute, url );
+router.mixins.push( timeout, named, regexpRoute, templateRoute, url );
 
 export default router;
